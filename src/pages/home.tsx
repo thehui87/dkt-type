50f95cf5ae300a38fcf99caf00d6c01cdb6b28aa
Debabrata H <thehui87@gmail.com>
2024-08-24 01:11:41 +0530

    caret position updates on every character but not on window resize

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,14 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, useLayoutEffect } from 'react';
 import { HeadingTag } from '../components/headingTag';
 import { RxReset } from 'react-icons/rx';
 import Tooltip from '../components/Tooltip';
 import Timer, { TimerHandle } from '../components/timer';
 import ConfigToolbar from '../components/toolbar';
 
+// const textBlock =
+//     'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt asperiores, dolorem nisi praesentium eligendi est consectetur ipsum veritatis fuga similique dolor magni obcaecati aut id, aperiam iure doloribus reprehenderit officia.';
 const textBlock =
-    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt asperiores, dolorem nisi praesentium eligendi est consectetur ipsum veritatis fuga similique dolor magni obcaecati aut id, aperiam iure doloribus reprehenderit officia.';
-
+    'Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody.';
 // interface textData {
 //     username: string;
 //     password: string;
@@ -25,17 +26,17 @@ interface CaretPos {
 }
 
 const Home = () => {
-    // const [textArray, setTextArray] = useState<Array<string>>([]);
     const [wordArray, setWordArray] = useState<Array<string>>([]);
     const [typedArray, setTypedArray] = useState<Array<string>>([]);
     const [activeWord, setActiveWord] = useState<string>('');
     const [activeWordHTML, setActiveWorHTML] = useState<Element | null>(null);
     const [inputValue, setInputValue] = useState('');
     const [counter, setCounter] = useState<number>(0);
-    const [spaceCounter, setSpaceCounter] = useState<number>(0);
     const [inputFocus, setInputFocus] = useState<boolean>(false);
-    const [isCorrect, setIsCorrect] = useState<Number>(0);
     const [letterArray, setLetterArray] = useState<Array<Element>>([]);
+    const [space, setSpace] = useState<boolean>(false);
+    // const [backspace, setBackspace] = useState<boolean>(false);
+    const [incorrectCounter, setIncorrectCounter] = useState<number>(0);
 
     const [isDisabled, setIsDisabled] = useState<boolean>(false);
     const [caretPosition, setCaretPosition] = useState<CaretPos>({
@@ -43,30 +44,43 @@ const Home = () => {
         y: 0,
     });
 
+    // const [offsetTopPosition, setOffsetTopPosition] = useState<number>(0);
+
     const timerRef = useRef<TimerHandle>(null);
+    const textContainerRef = useRef<HTMLDivElement>(null);
+    // const [size, setSize] = useState([0, 0]);
 
     const handleInputChange = (event: any) => {
         if (counter < wordArray.length) {
             timerRef.current?.start();
         }
 
-        setInputValue(event.target.value);
-
-        // caret placement
-        // let allCharacterLength = event.target.value.length;
-        // let onlyCharacters = event.target.value.replaceAll(' ', '').length;
-        // let spaces = allCharacterLength - onlyCharacters;
-
-        // setCounter(onlyCharacters);
-        // setSpaceCounter(spaces);
-        // let inputLength = event.target.value.trim().length;
-        // activeWordHTML.getEle
-
-        // if (event.target.value.trim().length > 0) {
-        // if(event.target.value.trim())
+        setInputValue(event.target.value.trim());
     };
 
     useEffect(() => {
+        // if (backspace) {
+        //     let index = inputValue.length - 1;
+        //     if (letterArray[index]) {
+        //         letterArray[index].classList.remove(
+        //             ...letterArray[index].classList
+        //         );
+        //         letterArray[index].classList.add('letter');
+        //     }
+        //     setBackspace(false);
+        // } else {
+
+        if (!space) {
+            for (
+                let i = inputValue.trim().length;
+                i < letterArray.length;
+                i++
+            ) {
+                letterArray[i].classList.remove(...letterArray[i].classList);
+                letterArray[i].classList.add('letter');
+            }
+        }
+
         inputValue
             .trim()
             .split('')
@@ -74,23 +88,29 @@ const Home = () => {
                 const isCorrect = value === activeWord[index];
                 if (letterArray[index]) {
                     if (isCorrect) letterArray[index].classList.add('correct');
-                    else letterArray[index].classList.add('incorrect');
+                    else {
+                        letterArray[index].classList.add('incorrect');
+                        setIncorrectCounter(incorrectCounter + 1);
+                    }
                 }
                 // console.log('Y: ', letterArray[index]);
 
                 // if (isCorrect) console.log('Y: ', letterArray[index]);
                 // else console.log('N:', letterArray[index]);
             });
+        updateCartePosition();
+
+        // }
     }, [inputValue]);
     const handleSpace = (e: any) => {
-        if (inputValue.length > 1 && inputValue.trim().length == 0) {
+        if (inputValue.length > 1 && inputValue.trim().length === 0) {
             setInputValue('');
         }
 
         if (e.keyCode === 32) {
             // console.log({ inputValue });
-            setTypedArray([...typedArray, inputValue]);
-            if (activeWord == inputValue.trim()) {
+            setTypedArray((oldArr) => [...oldArr, inputValue]);
+            if (activeWord === inputValue.trim()) {
                 wordValuation();
             } else {
                 wordValuation('error');
@@ -99,20 +119,60 @@ const Home = () => {
             setCounter((count) => {
                 return ++count;
             });
+            setSpace(true);
+        } else {
+            setSpace(false);
         }
         // backspace
-        if (e.keyCode === 8) {
-            let index = inputValue.length - 1;
-            if (letterArray[index]) {
-                letterArray[index].classList.remove(
-                    ...letterArray[index].classList
-                );
-                letterArray[index].classList.add('letter');
-            }
+        // if (e.keyCode === 8) {
+        //     setBackspace(true);
+        //     //     let index = inputValue.length - 1;
+        //     //     if (letterArray[index]) {
+        //     //         letterArray[index].classList.remove(
+        //     //             ...letterArray[index].classList
+        //     //         );
+        //     //         letterArray[index].classList.add('letter');
+        //     //     }
+        // } else {
+        //     setBackspace(false);
+        // }
+    };
+
+    const updateCartePosition = () => {
+        let boundingRect: DOMRect =
+            activeWordHTML?.getBoundingClientRect() as DOMRect;
+        // console.log({ boundingRect });
+        if (textContainerRef.current) {
+            let a: CaretPos = {
+                x:
+                    boundingRect?.left -
+                    textContainerRef.current.offsetLeft +
+                    inputValue.trim().length * 20 +
+                    7,
+                y: boundingRect?.top - textContainerRef.current.offsetTop + 10,
+            };
+            setCaretPosition(a);
+            // console.log('caretPos:', a);
         }
+        // console.log({ boundingRect });
+
+        // if (offsetTopPosition == 0) setOffsetTopPosition(boundingRect?.top);
     };
 
     useEffect(() => {
+        const setActiveWordHTML = () => {
+            let lastActiveWord = document.querySelector('.word.active');
+            if (lastActiveWord && lastActiveWord.classList.contains('active')) {
+                lastActiveWord.classList.remove('active');
+            }
+
+            let wordList = document.querySelectorAll('.word');
+            wordList[counter].classList.add('active');
+            setActiveWorHTML(wordList[counter]);
+            let letterArray = wordList[counter].querySelectorAll('.letter');
+            setLetterArray([...letterArray]);
+        };
+
         if (counter < wordArray.length) {
             setActiveWord(wordArray[counter]);
             setActiveWordHTML();
@@ -125,34 +185,43 @@ const Home = () => {
     useEffect(() => {
         setWordArray([...textBlock.split(' ')]);
         setIsDisabled(false);
-
+        setIncorrectCounter(0);
         return () => {
             timerRef.current?.reset();
         };
     }, []);
 
-    const resetBoard = () => {
-        setWordArray(() => [...textBlock.split(' ')]);
+    useLayoutEffect(() => {
+        // function updateSize() {
+        //   setSize([window.innerWidth, window.innerHeight]);
+        // }
+        window.addEventListener('resize', updateCartePosition);
+        updateCartePosition();
+        return () => window.removeEventListener('resize', updateCartePosition);
+    }, []);
 
+    const resetBoard = () => {
+        let lettersList = document.querySelectorAll('.letter');
+        for (let i = 0; i < lettersList.length; i++) {
+            lettersList[i].classList.remove(...lettersList[i].classList);
+            lettersList[i].classList.add('letter');
+        }
+
+        let wordsList = document.querySelectorAll('.word');
+        for (let i = 0; i < wordsList.length; i++) {
+            wordsList[i].classList.remove(...wordsList[i].classList);
+            wordsList[i].classList.add('word');
+        }
+
+        setWordArray(() => [...textBlock.split(' ')]);
         setCounter(0);
         setActiveWord(wordArray[0]);
         setInputValue('');
         setIsDisabled(false);
+        setTypedArray([]);
+        setIncorrectCounter(0);
 
         timerRef.current?.reset();
-    };
-
-    const setActiveWordHTML = () => {
-        let lastActiveWord = document.querySelector('.word.active');
-        if (lastActiveWord && lastActiveWord.classList.contains('active')) {
-            lastActiveWord.classList.remove('active');
-        }
-
-        let wordList = document.querySelectorAll('.word');
-        wordList[counter].classList.add('active');
-        setActiveWorHTML(wordList[counter]);
-        let letterArray = wordList[counter].querySelectorAll('.letter');
-        setLetterArray([...letterArray]);
     };
 
     const wordValuation = (value: string = '') => {
@@ -164,14 +233,7 @@ const Home = () => {
     };
 
     useEffect(() => {
-        let boundingRect: DOMRect =
-            activeWordHTML?.getBoundingClientRect() as DOMRect;
-
-        let a: CaretPos = {
-            x: boundingRect?.x - 136,
-            y: boundingRect?.bottom - boundingRect?.y - 30,
-        };
-        setCaretPosition(a);
+        updateCartePosition();
     }, [activeWordHTML]);
 
     const getWPM = () => {
@@ -214,17 +276,24 @@ const Home = () => {
                         </div>
                     </div>
                     {/* Text Field */}
-                    <div className="rounded-lg bg-teal-950 text-3xl text-teal-600 min-h-40 max-h-40 overflow-hidden py-2 leading-12 tracking-wider text-left relative">
-                        <div
-                            id="caret"
-                            className="bg-orange-400"
-                            style={{
-                                // left: `${7 + counter * 20 + spaceCounter * 16}px`,
-                                left: `${caretPosition.x}px`,
-                                top: `${caretPosition.y}px`,
-                                visibility: inputFocus ? 'visible' : 'hidden',
-                            }}
-                        ></div>
+                    <div
+                        ref={textContainerRef}
+                        className="rounded-lg bg-teal-950 text-3xl text-teal-600 min-h-40 max-h-40 overflow-hidden py-2 leading-12 tracking-wider text-left relative"
+                    >
+                        {!isDisabled && (
+                            <div
+                                id="caret"
+                                className="bg-orange-400 transition-all duration-75"
+                                style={{
+                                    // left: `${7 + counter * 20 + spaceCounter * 16}px`,
+                                    left: `${caretPosition.x}px`,
+                                    top: `${caretPosition.y}px`,
+                                    visibility: inputFocus
+                                        ? 'visible'
+                                        : 'hidden',
+                                }}
+                            ></div>
+                        )}
 
                         <input
                             className="opacity-0 absolute w-full h-full left-0 top-0"
@@ -265,7 +334,7 @@ const Home = () => {
                         </Tooltip>
                     </div>
                     {/* Stats */}
-                    <div className="flex flex-col">
+                    <div className="flex flex-col text-teal-100">
                         {isDisabled && (
                             <div className="flex flex-row ">
                                 {/* wordper minutes */}
@@ -286,12 +355,22 @@ const Home = () => {
                                     Characters: {typedArray.join('').length}/
                                     {wordArray.join('').length}
                                 </div>
+                                <div className={`${statsStyle}`}>
+                                    Wrong Key: {incorrectCounter}
+                                </div>
                                 {/* <div className={`${statsStyle}`}>
                                     Words: {typedArray.join("").length}/{wordArray.join("").length}
                                 </div> */}
                             </div>
                         )}
-                        <div>{isDisabled && typedArray.join(' ')}</div>
+                        <div className={'flex flex-col justify-start'}>
+                            <div className="text-left">
+                                {isDisabled && typedArray.join(' ')}
+                            </div>
+                            <div className="text-left">
+                                {isDisabled && wordArray.join(' ')}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
